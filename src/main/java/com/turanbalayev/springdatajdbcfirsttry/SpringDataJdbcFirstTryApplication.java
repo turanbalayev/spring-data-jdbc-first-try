92e9d75dd5fef1f8101eacfac88105e298465206
Turan Balayev <turanbalayev96@gmail.com>
2023-11-04 17:41:38 +0400

    First commit

---
+++ src/main/java/com/turanbalayev/springdatajdbcfirsttry/SpringDataJdbcFirstTryApplication.java
@@ -0,0 +1,13 @@
+package com.turanbalayev.springdatajdbcfirsttry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbcFirstTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJdbcFirstTryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turanbalayev/springdatajdbcfirsttry/SpringDataJdbcFirstTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turanbalayev.springdatajdbcfirsttry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcFirstTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
